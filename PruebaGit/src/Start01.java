18129c96e7bb7f776a7d71b2d7ea7bd05f1fd0ef
Mañana <Maana@DESKTOP-44OB301>
2022-12-12 13:24:27 +0100

    commit prueba

---
+++ PruebaGit/src/Start01.java
@@ -0,0 +1,23 @@
+
+public class Start01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("Hola mundo GIT");
+		//System.out.println("Adios mundo GIT");
+		//System.out.println("Aqui de nuevo GIT");
+		
+		
+		
+		
+		
+		
+		String a = "version1";
+		
+
+
+
+	}
+
+}
